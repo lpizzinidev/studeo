e1410abae9c0a9dbe5d1c6c6d500b87f0ae1b94d
Luca Pizzini <luca.pizzini@digitalidea.it>
2021-09-01 18:33:18 +0200

    React project setup
    Added CSS styles
    Added fonts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return <></>;
+  return (
+    <div className="container">
+      <h1 className="heading-1">Example</h1>
+      <p className="subtitle">This is roboto</p>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+// Fonts
+import "./fonts/Fredoka_One/FredokaOne-Regular.ttf";
+import "./fonts/Roboto/Roboto-Light.ttf";
+import "./fonts/Roboto/Roboto-Regular.ttf";
+import "./fonts/Roboto/Roboto-Medium.ttf";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
