93ef5229e8b584c8fe1bf5ee912ec24cd34561a6
Luca Pizzini <luca.pizzini@digitalidea.it>
2021-09-27 18:16:07 +0200

    Added categories editing and delete in detail

--- frontend/src/components/admin/categories/CategoriesContainer.js
+++ frontend/src/components/admin/categories/CategoriesContainer.js
@@ -1,25 +1,39 @@
 import { useState, useContext } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 
 import { CategoriesContext } from '../../../contexts/CategoriesContext';
 import { ResourcesContext } from '../../../contexts/ResourcesContext';
 
+import { CategoriesEditDialog } from './CategoriesEditDialog';
 import { ResourcesList } from '../resources/ResourcesList';
 import { ResourcesEditDialog } from '../resources/ResourcesEditDialog';
 import SearchBar from '../../views/SearchBar';
 import { Fab } from '../../views/Fab';
 
 import plusIcon from '../../../assets/icons/plus.svg';
+import editIcon from '../../../assets/icons/edit.svg';
+import deleteIcon from '../../../assets/icons/delete.svg';
 
 export const CategoriesContainer = () => {
   const { _id } = useParams();
+  const history = useHistory();
 
-  const { categories } = useContext(CategoriesContext);
+  const { categories, showEditCategory, deleteCategory } =
+    useContext(CategoriesContext);
   const { showEditResource } = useContext(ResourcesContext);
 
   const category = categories.find((category) => category._id === _id);
 
   const [search, setSearch] = useState('');
+
+  const handleEditCategory = () => {
+    showEditCategory(category);
+  };
+
+  const handleDeleteCategory = () => {
+    deleteCategory(_id);
+    history.goBack();
+  };
 
   const handleNewResource = () => {
     showEditResource(null);
@@ -28,14 +42,27 @@ export const CategoriesContainer = () => {
   return (
     <div>
       <SearchBar
-        placeholder="Search resource..."
+        placeholder='Search resource...'
         search={search}
         setSearch={setSearch}
       />
-      <h1 className="heading-1">{category.name}</h1>
+      <h1 className='heading-1'>{category.name}</h1>
+      <img
+        className='icon'
+        src={editIcon}
+        alt='Edit category'
+        onClick={handleEditCategory}
+      />
+      <img
+        className='icon'
+        src={deleteIcon}
+        alt='Delete category'
+        onClick={handleDeleteCategory}
+      />
       <ResourcesList search={search} />
+      <CategoriesEditDialog />
       <ResourcesEditDialog />
-      <Fab icon={plusIcon} alt="New resource" onClick={handleNewResource} />
+      <Fab icon={plusIcon} alt='New resource' onClick={handleNewResource} />
     </div>
   );
 };
