8e9e955f439d247aa7a23dfa242a8fa23c71a84f
Luca Pizzini <lpizzini7@gmail.com>
2021-10-18 17:17:54 +0200

    fix: adjusted test assertion

--- frontend/src/components/admin/categories/__test__/CategoriesEditDialog.test.js
+++ frontend/src/components/admin/categories/__test__/CategoriesEditDialog.test.js
@@ -5,6 +5,10 @@ import { CategoriesContext } from '../../../../contexts/CategoriesContext';
 
 import { CategoriesEditDialog } from '../CategoriesEditDialog';
 
+const testCategory = {
+  name: 'Example category name',
+};
+
 const MockCategoriesEditDialog = (props) => {
   return (
     <CategoriesContext.Provider
@@ -18,25 +22,21 @@ const MockCategoriesEditDialog = (props) => {
 };
 
 describe('CategoriesEditDialog', () => {
-  it('should display `New category` if no parameter are passed', () => {
-    render(<MockCategoriesEditDialog/>);
+  it('should display `New category` if no parameters are passed', () => {
+    render(<MockCategoriesEditDialog />);
 
     expect(screen.getByText('New category')).toBeVisible();
   });
 
   it('should display `Edit category` if editing category is passed', () => {
-    render(<MockCategoriesEditDialog editingCategory={{}} />);
+    render(<MockCategoriesEditDialog editingCategory={testCategory} />);
 
     expect(screen.getByText('Edit category')).toBeVisible();
   });
 
-    it('should display correct category name in editing field', () => {
-      const categoryName = 'Example category name';
-      
-      render(<MockCategoriesEditDialog editingCategory={{
-        name: categoryName
-      }} />);
+  it('should display correct category name in editing field', () => {
+    render(<MockCategoriesEditDialog editingCategory={testCategory} />);
 
-      expect(screen.getByRole('textbox', { value: categoryName })).toBeVisible();
-    });
+    expect(screen.getByDisplayValue(testCategory.name)).toBeVisible();
+  });
 });
