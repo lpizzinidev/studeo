50f965c336624e51bbda99aa74b53cbab1b1059c
Pizzo15 <lpizzini7@gmail.com>
2021-10-23 17:49:41 +0200

    test: added test ids

--- frontend/src/components/homepage/HomeNav.js
+++ frontend/src/components/homepage/HomeNav.js
@@ -4,10 +4,20 @@ const HomeNav = () => {
   return (
     <div className='home-navbar'>
       <Link to='/signin'>
-        <input type='button' className='text-button mr-15' value='LOG IN' />
+        <input
+          type='button'
+          className='text-button mr-15'
+          value='LOG IN'
+          data-testid='signin-button'
+        />
       </Link>
       <Link to='/signup'>
-        <input type='button' className='button' value='SIGN UP' />
+        <input
+          type='button'
+          className='button'
+          value='SIGN UP'
+          data-testid='signup-button'
+        />
       </Link>
     </div>
   );
