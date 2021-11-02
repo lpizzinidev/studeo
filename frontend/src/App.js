f80dafaa2c7bfcabde4985fa0441c26541ed6000
Luca Pizzini <lpizzini7@gmail.com>
2021-11-02 18:00:05 +0100

    added React error boundary

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Switch } from 'react-router-dom';
+import { ErrorBoundary } from 'react-error-boundary';
 
 import HomeContainer from './components/homepage/HomeContainer';
 import AuthContainer from './components/auth/AuthContainer';
 import DashboardContainer from './components/admin/dashboard/DashboardContainer';
 import { CategoriesContainer } from './components/admin/categories/CategoriesContainer';
 import { NotFound } from './components/pages/NotFound';
+import { ErrorFallback } from './components/pages/ErrorFallback';
 
 import { AuthProvider } from './contexts/AuthContext';
 import { CategoriesProvider } from './contexts/CategoriesContext';
@@ -15,27 +17,32 @@ import { PublicRoute } from './components/PublicRoute';
 
 function App() {
   return (
-    <AuthProvider>
-      <CategoriesProvider>
-        <ResourcesProvider>
-          <div className='container'>
-            <BrowserRouter>
-              <Switch>
-                <PublicRoute path='/' component={HomeContainer} exact />
-                <PublicRoute path='/signin' component={AuthContainer} />
-                <PublicRoute path='/signup' component={AuthContainer} />
-                <AdminRoute path='/dashboard' component={DashboardContainer} />
-                <AdminRoute
-                  path='/categories/:_id?'
-                  component={CategoriesContainer}
-                />
-                <AdminRoute component={NotFound} />
-              </Switch>
-            </BrowserRouter>
-          </div>
-        </ResourcesProvider>
-      </CategoriesProvider>
-    </AuthProvider>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <AuthProvider>
+        <CategoriesProvider>
+          <ResourcesProvider>
+            <div className='container'>
+              <BrowserRouter>
+                <Switch>
+                  <PublicRoute path='/' component={HomeContainer} exact />
+                  <PublicRoute path='/signin' component={AuthContainer} />
+                  <PublicRoute path='/signup' component={AuthContainer} />
+                  <AdminRoute
+                    path='/dashboard'
+                    component={DashboardContainer}
+                  />
+                  <AdminRoute
+                    path='/categories/:_id?'
+                    component={CategoriesContainer}
+                  />
+                  <AdminRoute component={NotFound} />
+                </Switch>
+              </BrowserRouter>
+            </div>
+          </ResourcesProvider>
+        </CategoriesProvider>
+      </AuthProvider>
+    </ErrorBoundary>
   );
 }
 
--- frontend/src/components/homepage/HomeContainer.js
+++ frontend/src/components/homepage/HomeContainer.js
@@ -1,12 +1,12 @@
-import HomeNav from "./HomeNav";
-import HomeHeading from "./HomeHeading";
-import HomeLogo from "./HomeLogo";
+import HomeNav from './HomeNav';
+import HomeHeading from './HomeHeading';
+import HomeLogo from './HomeLogo';
 
 const HomeContainer = () => {
   return (
     <div>
       <HomeNav />
-      <div className="home-container">
+      <div className='home-container'>
         <HomeHeading />
         <HomeLogo />
       </div>
