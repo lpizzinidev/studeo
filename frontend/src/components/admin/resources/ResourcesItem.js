a71782bf23a17f19dec2828886d30d993f017a46
Luca Pizzini <lpizzini7@gmail.com>
2021-11-02 18:24:06 +0100

    added resource delete action

--- frontend/src/components/admin/resources/ResourcesItem.js
+++ frontend/src/components/admin/resources/ResourcesItem.js
@@ -1,14 +1,28 @@
-import { useContext } from 'react';
+import { useState, useContext } from 'react';
 
 import { ResourcesContext } from '../../../contexts/ResourcesContext';
 
+import { DialogConfirm } from '../../views/DialogConfirm';
+
 import * as utils from '../../../util/util';
 
+import deleteIcon from '../../../assets/icons/delete.svg';
+
 export const ResourcesItem = ({ resource }) => {
+  const [showDialogDelete, setShowDialogDelete] = useState(false);
+
   const { showEditResource } = useContext(ResourcesContext);
 
   const handleClick = () => {
     showEditResource(resource);
+  };
+
+  const requestDeleteResource = () => {
+    setShowDialogDelete(true);
+  };
+
+  const handleDeleteResource = () => {
+    setShowDialogDelete(false);
   };
 
   return (
@@ -23,7 +37,20 @@ export const ResourcesItem = ({ resource }) => {
           {utils.formatDuration(resource.duration)}
         </p>
         <p className='text-body-1'>{resource.name}</p>
+        <img
+          src={deleteIcon}
+          alt='Delete resource'
+          className='icon'
+          onClick={requestDeleteResource}
+        />
       </div>
+      <DialogConfirm
+        title='Delete resource'
+        message='Are you sure you want to delete the resource?'
+        confirmAction={handleDeleteResource}
+        show={showDialogDelete}
+        setShow={setShowDialogDelete}
+      />
     </div>
   );
 };
